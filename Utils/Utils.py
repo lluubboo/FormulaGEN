a644d2c8a3de3cbe387c5fe64f7c2fcd2d519d39
lubomir.balaz <lubomirkobalaz@gmail.com>
2023-06-19 23:34:08 +0200

    project renamed & scope changed

--- Utils/Utils.py
+++
@@ -1,8 +0,0 @@
-import tabulate
-
-
-class TablePrinter:
-
-    @staticmethod
-    def printTable(data):
-        print("\n" + tabulate.tabulate(data, headers=["Entity ID", "Specific ID", "Value ID", "Entity name"]))
